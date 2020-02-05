f3532ee8f59f7b9fa1d6cad7ffe4688fee85a9af
KrusidOoO <57525539+KrusidOoO@users.noreply.github.com>
2020-02-05 08:23:59 +0100

    iadfdagdsg

--- SubnetCalculator/Program.cs
+++ SubnetCalculator/Program.cs
@@ -26,18 +26,63 @@ namespace SubnetCalculator
             int n, i;
             int[] a = new int[10];
 
-            Console.Write("Enter the number to convert: ");
             n = int.Parse(IpWithoutDots[0]);
             for(i=0;n>0;i++)
             {
                 a[i] = n % 2;
                 n = n / 2;
             }
-            Console.Write("Binary of the given number is = ");
+            Console.WriteLine("Binary of the given number is = ");
             for(i=i-1;i>=0;i--)
             {
                 Console.Write(a[i]);
             }
+            Console.WriteLine();
+            int b, c;
+            int[] d = new int[10];
+
+            b = int.Parse(IpWithoutDots[1]);
+            for (c = 0; b > 0; c++)
+            {
+                d[c] = b % 2;
+                b = b / 2;
+            }
+            Console.WriteLine("Binary of the given number is = ");
+            for (c = c - 1; c >= 0; c--)
+            {
+                Console.Write(d[c]);
+            }
+            Console.WriteLine();
+            int e, f;
+            int[] g = new int[10];
+
+            e = int.Parse(IpWithoutDots[2]);
+            for (f = 0; e > 0; f++)
+            {
+                g[f] = e % 2;
+                e = e / 2;
+            }
+            Console.WriteLine("Binary of the given number is = ");
+            for (f = f - 1; f >= 0; f--)
+            {
+                Console.Write(g[f]);
+            }
+            Console.WriteLine();
+            int h, j;
+            int[] k = new int[10];
+
+            h = int.Parse(IpWithoutDots[3]);
+            for (j = 0; h > 0; j++)
+            {
+                k[j] = h % 2;
+                h = h / 2;
+            }
+            Console.WriteLine("Binary of the given number is = ");
+            for (j = j - 1; j >= 0; j--)
+            {
+                Console.Write(k[j]);
+            }
+            Console.WriteLine();
 
             Console.ReadKey();
   
